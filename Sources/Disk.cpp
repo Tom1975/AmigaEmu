8ca8ea140f24bd9be4e4a107c1999ded572948b4
Thomas Guillemin <thomas.guillemin@conduent.com>
2022-03-22 17:31:06 +0100

    ADF & MFM decoding

--- Sources/Disk.cpp
+++ Sources/Disk.cpp
@@ -1,5 +1,50 @@
 #include "Disk.h"
 
+
+///////////////////////////////////////////////////////////
+// Track
+Track::Track():bitstream_(nullptr), size_(0)
+{
+
+}
+
+Track::~Track()
+{
+   Clear();
+}
+
+void Track::Clear()
+{
+   delete[]bitstream_;
+   bitstream_ = nullptr;
+   size_ = 0;
+}
+
+
+///////////////////////////////////////////////////////////
+// Side
+Side::Side():nb_tracks_(0), track_(nullptr)
+{
+
+}
+
+Side::~Side()
+{
+   Clear();
+}
+
+void Side::Clear()
+{
+   for (int i = 0; i < nb_tracks_; i++)
+   {
+      track_[i].Clear();
+   }
+   delete[]track_;
+}
+
+
+///////////////////////////////////////////////////////////
+// Disk
 Disk::Disk() : valid_(false)
 {
 
@@ -13,6 +58,12 @@ Disk::Disk(std::string filepath)
 Disk::~Disk()
 {
 
+}
+
+void Disk::ClearStructure()
+{
+   side_[0].Clear();
+   side_[1].Clear();
 }
 
 bool Disk::LoadFile(std::string filepath)
@@ -40,13 +91,172 @@ bool Disk::LoadFile(std::string filepath)
 }
 
 
+int Disk::AddMFMByteToTrack(unsigned char* track, unsigned int index, unsigned short mfmbyte, int size,
+   unsigned char previous_bit)
+{
+   if (track == nullptr)
+      return index + (size );
+
+   if (index > 0)
+   {
+      previous_bit = track[index - 1];
+   }
+
+   for (int i = 0; i < size; i++)
+   {
+      // Get bit
+      unsigned char c = ((mfmbyte >> (size - 1 - i)) & 0x01);
+
+      track[index++] = c;
+      previous_bit = c;
+   }
+   return index;
+}
+
+int Disk::AddByteToTrack(unsigned char* track, unsigned int index, unsigned char byte, int size,
+   unsigned char previous_bit)
+{
+   if (track == nullptr)
+      return index + (size * 2);
+
+   if (index > 0)
+   {
+      previous_bit = track[index - 1];
+   }
+
+   for (int i = 0; i < size; i++)
+   {
+      // Get bit
+      unsigned char c = ((byte >> (7 - i)) & 0x01);
+
+      if (c == 1)
+      {
+         track[index++] = 0;
+         track[index] = 1;
+      }
+      else
+      {
+         track[index++] = (previous_bit == 0) ? 1 : 0;
+         track[index] = 0;
+      }
+
+      previous_bit = track[index++];
+   }
+   return index;
+}
+
+int Disk::AddLongToTrackOddEven(unsigned char* track, unsigned int index, unsigned long *data, size_t nb_data)
+{
+   if (track == nullptr)
+      return index + 64;
+
+   unsigned char previous_bit = 0;
+   if (index > 0)
+   {
+      previous_bit = track[index - 1];
+   }
+
+   for (int j = 0; j < 2; j++)
+   {
+      for (size_t k = 0; k < nb_data; k++)
+      {
+         for (int i = 0; i < 16; i++)
+         {
+            // Get bit
+            unsigned char c = ((data[k] >> (31 - (i * 2) - j)) & 0x01);
+
+            if (c == 1)
+            {
+               track[index++] = 0;
+               track[index] = 1;
+            }
+            else
+            {
+               track[index++] = (previous_bit == 0) ? 1 : 0;
+               track[index] = 0;
+            }
+
+            previous_bit = track[index++];
+         }
+      }
+   }
+   return index;
+}
+
+size_t Disk::AddCylinderFromSectorList(Track* track, unsigned char track_number, size_t nb_sectors, size_t index, unsigned char* buffer_in, size_t buffer_size)
+{
+   track->size_ = 0x19000;
+   track->bitstream_ = new unsigned char[track->size_];
+
+   // add sectors
+   size_t stream_index = 0;
+   for (size_t s = 0; s < nb_sectors; s++)
+   {
+      // SYNC bits : 0xAAAAAAAA
+      stream_index = AddMFMByteToTrack(track->bitstream_, stream_index, 0xAAAA);
+      stream_index = AddMFMByteToTrack(track->bitstream_, stream_index, 0xAAAA);
+      // SYNC Word : 0x4489 0x4489
+      stream_index = AddMFMByteToTrack(track->bitstream_, stream_index, 0x4489);
+      stream_index = AddMFMByteToTrack(track->bitstream_, stream_index, 0x4489);
+
+      // HEADER : 
+      unsigned long header_info = 0xFF0000 | (track_number << 16) | ((s & 0xFF) << 8) | ((nb_sectors - s) & 0xFF);
+      stream_index = AddLongToTrackOddEven(track->bitstream_, stream_index, &header_info, 1);
+
+      // Label
+      unsigned long label[] = { 0x0, 0x0 , 0x0 , 0x0 };
+      stream_index = AddLongToTrackOddEven(track->bitstream_, stream_index, label, 4);
+
+      // Checksum
+      // Data checksum
+      // Data
+   }
+
+   return stream_index;
+}
+
+
 
 bool Disk::LoadADF(unsigned char* buffer, size_t size)
 {
    // Load ADF :
    // - Check size
-   // - Size ok, load each track/side
+   size_t nb_sectors_total = size / 512;
 
+   // - Size ok, load each track/side
+   // todo : handle HD
+   size_t nb_sectors = 11;
+   size_t nb_tracks = nb_sectors_total / 11;
+   nb_sides_ = (nb_tracks > 80) ? 2 : 1;
+   size_t nb_cylinders;
+   if (nb_sides_ == 1)
+   {
+      nb_cylinders = nb_tracks;
+   }
+   else
+   {
+      nb_cylinders = nb_tracks / 2;
+   }
+
+   // Create structure.
+   ClearStructure();
+
+   for (size_t s = 0; s < nb_sides_; s++)
+   {
+      side_[s].nb_tracks_ = nb_cylinders;
+      side_[s].track_ = new Track[nb_cylinders];
+   }
+
+   size_t index = 0;
+   for (size_t c = 0; c < nb_cylinders; c++)
+   {
+      for (size_t s = 0; s < nb_sides_; s++)
+      {
+         index = AddCylinderFromSectorList(&side_[s].track_[c], (c*2+s), nb_sectors, index, buffer, size);
+         side_[s].track_[c].bitstream_ = new unsigned char[512];
+         side_[s].track_[c].size_ = 512;
+      }
+   }
 
 
    return true;
--- Sources/Disk.h
+++ Sources/Disk.h
@@ -5,6 +5,12 @@
 class Track
 {
 public:
+   Track();
+   virtual ~Track();
+
+   void Clear();
+
+
    unsigned char* bitstream_;
    size_t size_;
 };
@@ -12,7 +18,14 @@ public:
 class Side
 {
 public:
-   Track track_[80];
+   Side();
+   virtual ~Side();
+
+   void Clear();
+
+
+   Track *track_;
+   size_t nb_tracks_;
 
 };
 
@@ -28,9 +41,14 @@ public:
    bool IsValid() { return valid_; }
 
 private: 
-
+   void ClearStructure();
+   size_t AddCylinderFromSectorList(Track* track, unsigned char track_number, size_t nb_sectors, size_t index, unsigned char* buffer_in, size_t buffer_size);
+   int AddMFMByteToTrack(unsigned char* track, unsigned int index, unsigned short mfmbyte, int size = 16, unsigned char previous_bit = 0);
+   int AddByteToTrack(unsigned char* track, unsigned int index, unsigned char byte, int size = 8, unsigned char previous_bit = 0);
+   int AddLongToTrackOddEven(unsigned char* track, unsigned int index, unsigned long *data, size_t nb_data);
    bool valid_;
 
    Side side_[2];
+   size_t nb_sides_;
 
 };
