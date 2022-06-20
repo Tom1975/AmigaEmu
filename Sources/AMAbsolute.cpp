f2d6c394dc9204645d8e131a5e91d0e6296ed310
Thomas Guillemin <thomas.guillemin@gmail.com>
2022-06-20 23:08:58 +0200

    Fix supq (absolute). Need some tests

--- Sources/AMAbsolute.cpp
+++ Sources/AMAbsolute.cpp
@@ -161,7 +161,31 @@ unsigned short AMAbsolute::WriteNextWord(unsigned int& address_to_write)
 // Do somme math !
 void AMAbsolute::Subq(unsigned char data, unsigned char size, unsigned short& sr)
 {
+   unsigned int new_value;
+   unsigned int old_value;
+   unsigned int mask;
+   switch (size_)
+   {
+   case Byte:
+      old_value = GetU8();
+      mask = 0xFF;
+      break;
+   case Word:
+      old_value = GetU16();
+      mask = 0xFFFF;
+      break;
+   case Long:
+      old_value = GetU32();
+      mask = 0xFFFFFFFF;
+      break;
+   }
+   
+   new_value = (old_value - data) & mask;
+
+   ComputeFlags(sr, old_value, new_value, data);
+
    // Nothing to do here
+   WriteInput(new_value);
 }
 
 void AMAbsolute::Add(AddressingMode* source, unsigned short& sr)
